84af78eda93e39b5de598dc2c3a65a1fd4b5768c
Jibidus <jibidus@gmail.com>
2015-07-15 08:48:22 +0200

    Rename column Character.TEXT to Character.NAME

--- app/models/Characters.scala
+++ app/models/Characters.scala
@@ -2,11 +2,10 @@ package models
 
 import play.api.db.slick.Config.driver.simple._
 
-case class Character(id: Long, text: String)
+case class Character(id: Long, name: String)
 
 class Characters(tag: Tag) extends Table[Character](tag, "Character") {
   def id = column[Long]("ID", O.PrimaryKey, O.AutoInc)
-  def text = column[String]("TEXT")
-  def * = (id, text) <> (Character.tupled, Character.unapply _)
+  def name = column[String]("NAME")
+  def * = (id, name) <> (Character.tupled, Character.unapply _)
 }
-//val characters = TableQuery[Characters]
