8489e82132f9a8b279c6761337ff5356606c61c2
Jibidus <jibidus@gmail.com>
2015-12-20 11:18:13 +0100

    Remove unnecessary trait

--- src/main/scala/jibidus/kaamelott/server/http/RestInterface.scala
+++ src/main/scala/jibidus/kaamelott/server/http/RestInterface.scala
@@ -6,12 +6,10 @@ import spray.routing._
 import scala.concurrent.ExecutionContext
 import scala.language.postfixOps
 
-class RestInterface(implicit val executionContext: ExecutionContext) extends HttpServiceActor with Resources {
+class RestInterface(implicit val executionContext: ExecutionContext) extends HttpServiceActor with CharactersResource {
 
   def receive = runRoute(routes)
 
   val routes: Route = charactersRoutes
 
 }
-
-trait Resources extends CharactersResource
